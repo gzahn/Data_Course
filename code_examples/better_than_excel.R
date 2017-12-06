db7881efa70f716c66f0efc188ff4fb437c5c9c1
gzahn <geoff.gone@gmail.com>
2017-12-05 18:20:13 -0700

    1.0

--- code_examples/better_than_excel.R
+++ code_examples/better_than_excel.R
@@ -19,7 +19,24 @@ summary(wingspan_vs_mass$mass)[c(1,3,6)]
 # Plot wingspan vs mass
 plot(wingspan_vs_mass$wingspan ~ wingspan_vs_mass$mass)
 
+# Get linear model fit
+fit = lm(wingspan_vs_mass$wingspan ~ wingspan_vs_mass$mass)
+# Get slope (coefficient of mass)
+fit$coefficients[2]
+
 # Print a file that contains the measured mass values, ordered from highest to lowest (one value per line)
-sink("./code_examples/mass_ordered.txt")
+sink("./output/mass_ordered.txt")
 cat(wingspan_vs_mass$mass, sep = "\n")
 sink(NULL)
+
+# Print a file that has the slope of our linear model fit (cofficient of mass for linear model) 
+# along with summary stats for each variable
+sink("./output/summary_and_slope.txt")
+print("Wingspan",quote = FALSE)
+summary(wingspan_vs_mass$wingspan)[c(1,3,6)]
+print("Mass",quote = FALSE)
+summary(wingspan_vs_mass$mass)[c(1,3,6)]
+print("Slope of linear model fit",quote = FALSE)
+fit$coefficients[2]
+sink(NULL)
+
