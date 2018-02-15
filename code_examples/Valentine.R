b14f25108f4802a7d818689f5ae349e743ee8b0b
gzahn <geoff.gone@gmail.com>
2018-02-15 08:44:56 -0700

    ggplot valentine

---
+++ code_examples/Valentine.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(ggplot2)
+data_frame(t=seq(-pi, 0, .001),
+           x1=16*(sin(t))^2,
+           x2=-x1,
+           y=13*cos(t) -5 * cos(2*t) -2*cos(3*t) - cos(4*t)) %>%
+  gather(side, x, x1, x2) %>%
+  ggplot(aes(x,y)) +
+  geom_polygon(fill="red") +
+  coord_fixed() +
+  theme_minimal() 
