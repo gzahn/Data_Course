fa536b7d8b9adf81e3df14b69d1074d044962bea
gzahn <geoff.gone@gmail.com>
2018-12-31 07:49:24 -0700

    SP_2019

--- Assignments/Assignment_6/Assignment_6_messy_code.R
+++ Assignments/Assignment_6/Assignment_6_messy_code.R
@@ -24,6 +24,43 @@ buddhist = buddhist[order(buddhist$Pop_2010, decreasing = TRUE),]
 write.csv(buddhist, file = "./buddhist_counties.csv", row.names = FALSE, quote = FALSE)
 
 
+## get group summaries of religiousity based on population ##
+
+# divide each county into one of six groups based on populations
+# note: keep these two lines the same in your updated code!
+groups = kmeans(utah$Pop_2010,6) # clusters data into 6 groups based on proximity to mean of potential groups
+utah$Pop.Group = groups$cluster # assigns a new variable to utah giving group for each county
+
+# subset to each group and find summary stats on Religiosity for each
+group1 = mean(utah[utah$Pop.Group == 1,]$Religious)
+group2 = mean(utah[utah$Pop.Group == 2,]$Religious)
+group3 = mean(utah[utah$Pop.Group == 3,]$Religious)
+group4 = mean(utah[utah$Pop.Group == 4,]$Religious)
+group5 = mean(utah[utah$Pop.Group == 5,]$Religious)
+group6 = mean(utah[utah$Pop.Group == 6,]$Religious)
+
+# same, but mean population
+group1.pop = mean(utah[utah$Pop.Group == 1,]$Pop_2010)
+group2.pop = mean(utah[utah$Pop.Group == 2,]$Pop_2010)
+group3.pop = mean(utah[utah$Pop.Group == 3,]$Pop_2010)
+group4.pop = mean(utah[utah$Pop.Group == 4,]$Pop_2010)
+group5.pop = mean(utah[utah$Pop.Group == 5,]$Pop_2010)
+group6.pop = mean(utah[utah$Pop.Group == 6,]$Pop_2010)
+
+
+# make data frame of each group and mean religiosity
+religiosity = data.frame(Pop.Group = c("group1","group2","group3","group4","group5","group6"),
+           Mean.Religiosity = c(group1,group2,group3,group4,group5,group6),
+           Mean.Pop = c(group1.pop,group2.pop,group3.pop,group4.pop,group5.pop,group6.pop))
+
+religiosity # take quick look at resulting table
+
+# order by decreasing population
+religiosity = religiosity[order(religiosity$Mean.Pop, decreasing = TRUE),]
+
+religiosity # take quick look at resulting table
+
+
 #####################################
 #              Part 2               #
 # Beginning to look at correlations #
